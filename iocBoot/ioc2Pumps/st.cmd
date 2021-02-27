145157bfb34ecb0559d0cb14894a65be8184e0b4
Mark Rivers <rivers@cars.uchicago.edu>
2021-02-27 11:51:54 -0600

    Use environment variables for poll and timeout; add autosave

--- iocBoot/ioc2Pumps/st.cmd
+++ iocBoot/ioc2Pumps/st.cmd
@@ -15,6 +15,8 @@ SPApp_registerRecordDeviceDriver(pdbbase)
 epicsEnvSet("PREFIX", "ISCO1:")
 
 epicsEnvSet("PORT", "SP1")
+epicsEnvSet("POLL_MS", "1000")
+epicsEnvSet("TIMEOUT_MS", "10000")
 
 # Use the following commands for TCP/IP
 #drvAsynIPPortConfigure(const char *portName, 
@@ -24,14 +26,16 @@ epicsEnvSet("PORT", "SP1")
 #                       int noProcessEos);
 
 # Change IP address for your device
-drvAsynIPPortConfigure("$(PORT)","gse-isco1:502",0,0,0)
+drvAsynIPPortConfigure("$(PORT)", "gse-isco1:502", 0, 0, 0)
+
+#asynSetOption($(PORT), 0, "disconnectOnReadTimeout", "Y")
 
 #modbusInterposeConfig(const char *portName, 
 #                      modbusLinkType linkType,
 #                      int timeoutMsec,
 #                      int writeDelayMsec)
 
-modbusInterposeConfig("$(PORT)",0,10000,10)
+modbusInterposeConfig("$(PORT)", 0, $(TIMEOUT_MS), 0)
 
 ### The syringe pump supports the following modbus function codes:
 #    01 - read discrete output coils
@@ -52,19 +56,19 @@ modbusInterposeConfig("$(PORT)",0,10000,10)
 #   char *plcType)
 
 # Access 142 bits (0-141) as inputs. Function code=1.
-drvModbusAsynConfigure("$(PORT)_Bit_In",  "$(PORT)", 1, 1,  0, 142, 0, 100, "Teledyne")
+drvModbusAsynConfigure("$(PORT)_Bit_In",  "$(PORT)", 1, 1,  0, 142, 0, $(POLL_MS), "Teledyne")
 
 # Access 109 bits (0-108) as outputs. Function code=5.
 drvModbusAsynConfigure("$(PORT)_Bit_Out", "$(PORT)", 1, 5,  0, 109, 0, 1, "Teledyne")
 
 # Access first set of 100 16-bit holding registers starting at 0 as inputs. Function code=3. Data type=FLOAT32_BE
-drvModbusAsynConfigure("$(PORT)_Reg_In_1",  "$(PORT)", 1,  3, 0, 100, FLOAT32_BE, 100, "Teledyne")
+drvModbusAsynConfigure("$(PORT)_Reg_In_1",  "$(PORT)", 1,  3, 0, 100, FLOAT32_BE, $(POLL_MS), "Teledyne")
 
 # Access second set of 62 16-bit holding registers starting at 100 as inputs. Function code=3. Data type=FLOAT32_BE
-drvModbusAsynConfigure("$(PORT)_Reg_In_2",  "$(PORT)", 1,  3, 100, 62, FLOAT32_BE, 100, "Teledyne")
+drvModbusAsynConfigure("$(PORT)_Reg_In_2",  "$(PORT)", 1,  3, 100, 62, FLOAT32_BE, $(POLL_MS), "Teledyne")
 
 # Access third set of 46 16-bit holding registers starting at 200 as inputs. Function code=3. Data type=FLOAT32_BE
-drvModbusAsynConfigure("$(PORT)_Reg_In_3",  "$(PORT)", 1,  3, 200, 46, FLOAT32_BE, 100, "Teledyne")
+drvModbusAsynConfigure("$(PORT)_Reg_In_3",  "$(PORT)", 1,  3, 200, 46, FLOAT32_BE, $(POLL_MS), "Teledyne")
 
 # Access first set of 100 16-bit holding registers starting at 0 as outputs. Function code=16. Data type=FLOAT32_BE
 drvModbusAsynConfigure("$(PORT)_Reg_Out_1",  "$(PORT)", 1,  16, 0, 100, FLOAT32_BE, 1, "Teledyne")
@@ -86,11 +90,15 @@ dbLoadRecords("$(TOP)/db/ISCOPumpN.template", "P=$(PREFIX), PUMP=A:")
 dbLoadRecords("$(TOP)/db/ISCOPumpN.template", "P=$(PREFIX), PUMP=B:")
 
 # Enable ASYN_TRACEIO_HEX on octet server
-asynSetTraceIOMask("$(PORT)",0,4)
+asynSetTraceIOMask("$(PORT)", 0, HEX)
 
 # Enable ASYN_TRACE_ERROR and ASYN_TRACEIO_DRIVER on octet server
-#asynSetTraceMask("$(PORT)",0,9)
+#asynSetTraceMask("$(PORT)", 0, ERROR|DRIVER)
+
+<../save_restore_IOCSH.cmd
 
 ###############################################################################
 iocInit
 ###############################################################################
+
+create_monitor_set("auto_settings.req",30,"P=$(PREFIX)")
