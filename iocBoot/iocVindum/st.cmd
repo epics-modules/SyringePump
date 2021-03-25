3f9b41b9c09142be385a8bc6b0ff2f720fe27e33
Mark Rivers <rivers@cars.uchicago.edu>
2021-03-25 09:04:19 -0500

    Additional substitutions and template files

--- iocBoot/iocVindum/st.cmd
+++ iocBoot/iocVindum/st.cmd
@@ -12,7 +12,7 @@ dbLoadDatabase("../../dbd/SPApp.dbd")
 SPApp_registerRecordDeviceDriver(pdbbase)
 
 # prefix used for all PVs in this IOC
-epicsEnvSet("PREFIX", "VINDUM1:")
+epicsEnvSet("PREFIX", "VINDUM1:P1:")
 
 epicsEnvSet("PORT", "SP1")
 epicsEnvSet("POLL_MS", "1000")
@@ -67,11 +67,25 @@ modbusInterposeConfig("$(PORT)", 0, $(TIMEOUT_MS), 0)
 #   int pollMsec,
 #   char *plcType)
 
-# Access first set of 63 16-bit holding registers starting at 2 as inputs. Function code=3. Data type=UINT16
-drvModbusAsynConfigure("$(PORT)_Reg_In_1",  "$(PORT)", 1,  4, 0, 65, UINT16, $(POLL_MS), "Anybus")
+# Access 124 16-bit input registers starting at 0 as inputs. Function code=3. Default data type=UINT16
+drvModbusAsynConfigure("$(PORT)_Reg_In_1", "$(PORT)", 1, 4, 0, 124, UINT16, $(POLL_MS), "Anybus")
+#asynSetTraceIOMask("$(PORT)_Reg_In_1", 0, HEX)
+#asynSetTraceMask("$(PORT)_Reg_In_1", 0, 255)
 
-# Load the substitutions files for the records that use Modbus
-dbLoadTemplate("$(TOP)/db/VindumReadAll.substitutions", "P=$(PREFIX)P1:, PORT=$(PORT)_Reg_In_1")
+# Access 25 16-bit output registers starting at 0 as outputs. Function code=16. Default data type=UINT16
+# Note that with this function code these map to Asynbus addresss starting at 0x200.
+drvModbusAsynConfigure("$(PORT)_Reg_Out_1",  "$(PORT)", 1, 16, 0, 25, UINT16, 1, "Anybus")
+
+# Load the substitutions files for the ReadAll Modbus records
+dbLoadTemplate("$(TOP)/db/VindumReadAll.substitutions", "P=$(PREFIX), PORT=$(PORT)_Reg_In_1")
+
+# Load the substitutions files for the PumpCommands Modbus records
+dbLoadTemplate("$(TOP)/db/VindumPumpCommands.substitutions", "P=$(PREFIX), PORT=$(PORT)_Reg_Out_1")
+
+# Load the database files for the other records
+dbLoadRecords("$(TOP)/db/VindumController.template", "P=$(PREFIX)")
+dbLoadRecords("$(TOP)/db/VindumPumpN.template", "P=$(PREFIX), PUMP=A:")
+dbLoadRecords("$(TOP)/db/VindumPumpN.template", "P=$(PREFIX), PUMP=B:")
 
 # Load an asyn record for debugging
 dbLoadRecords("$(ASYN)/db/asynRecord.db", "P=$(PREFIX), R=asyn1, PORT=$(PORT), ADDR=0, IMAX=256, OMAX=256")
